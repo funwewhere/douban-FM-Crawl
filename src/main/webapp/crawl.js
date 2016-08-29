cd0815ad3424084803859f833d06ecfc88e4132a
funwewhere <funwewhere@funwewhere-linux.HOST>
2016-08-29 23:08:42 +0800

    修改：     src/main/webapp/crawl.js

--- src/main/webapp/crawl.js
+++ src/main/webapp/crawl.js
@@ -1,30 +1,34 @@
-function getSongInfo(){
-	var data = "cookieString="+encodeURIComponent($("input[name=cookieString]", ".form").val());
-	$.ajax({
-        type: "POST",
-        url: "getSongInfo",
-        data: data,
-        dataType: "json",
-        success: function(data){
-        	console.log(data);
-        	var html = "";
-        	for(var i = 0; i < data.length; ++i){
-        		var minute =  Math.floor(data[i].length/60);
-        		var second = data[i].length%60;
-        		minute = fillPosition(2, minute);
-        		second = fillPosition(2, second);
-        		var artist = data[i].artist;
-        		html += "<tr><td><span class='sn'>" + (i+1) +"</span></td><td>" + data[i].title + "</td><td><div class='artist text'><span title='"+ data[i].artist +"'>" + data[i].artist + "</span></div></td><td>" + minute + ":" + second + "</td><td><a href='" + data[i].url + "' rel='nofollow'>试   听</a></td></tr>";
-        	}
-        	$(".data table").append(html);
-        }
-    }); 
+
+
+DoubanCrawl.prototype = {
+    getSongInfo:function(cookieString){
+		var data = "cookieString="+encodeURIComponent(cookieString);
+		$.ajax({
+	        type: "POST",
+	        url: "getSongInfo",
+	        data: data,
+	        dataType: "json",
+	        success: function(data){
+	        	console.log(data);
+	        	var html = "";
+	        	for(var i = 0; i < data.length; ++i){
+	        		var minute =  Math.floor(data[i].length/60);
+	        		var second = data[i].length%60;
+	        		minute = this.fillPosition(2, minute);
+	        		second = this.fillPosition(2, second);
+	        		var artist = data[i].artist;
+	        		html += "<tr class='" + (i % 2 == 0?'even':'') + "'><td><span class='sn'>" + (i+1) +"</span></td><td>" + data[i].title + "</td><td><div class='artist text'><span title='"+ data[i].artist +"'>" + data[i].artist + "</span></div></td><td>" + minute + ":" + second + "</td><td><a href='" + data[i].url + "' target='_Blank'>试   听</a></td></tr>";
+	        	}
+	        	$(".data table").append(html);
+	        }
+	    }); 
+	},
+	fillPosition:function(pn, num){
+		num = num.toString();
+		while(num.length < pn){
+			num = "0" + num;
+		}
+		return num;
+	}
 }
 
-function fillPosition(pn, num){
-	num = num.toString();
-	while(num.length < pn){
-		num = "0" + num;
-	}
-	return num;
-}
