1ff492e453cf882808df3e64922048dd615a365b
fww <funwewhere@foxmail.com>
2016-10-11 14:47:43 +0800

    clear

--- src/main/java/com/funwewhere/doubanfmcrawl/util/DownLoadThread.java
+++ src/main/java/com/funwewhere/doubanfmcrawl/util/DownLoadThread.java
@@ -9,9 +9,7 @@ import com.funwewhere.doubanfmcrawl.bean.SongInfo;
 import com.funwewhere.doubanfmcrawl.http.WebRequestUtil;
 
 public class DownLoadThread implements Runnable {
-	
-//	private static List<SongInfo> songs = null;
-	
+
 	private BlockingQueue<SongInfo> songQueue;
 	
 	private String savePath;
@@ -52,45 +50,5 @@ public class DownLoadThread implements Runnable {
 		}
 		System.out.println(Thread.currentThread().getName() + "下载了" + i + "首歌！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！");
 	}
-	
-//	@Override
-//	public void run() {
-//		int i = 0;
-//		while(true){
-//			boolean isfind = false;
-//			for(SongInfo song : songs){
-//				if(song.isDownloaded()){
-//					continue;
-//				}
-//				synchronized (song) {
-//					if(!song.isDownloaded()){
-//						++i;
-//						isfind = true;
-//						song.setDownloaded(true);
-//						String downloadUrl = song.getUrl();
-//						String fileName = song.getArtist() + " - " + song.getTitle();
-//						if(song.isHaveRepeatName()){
-//							fileName += " " + song.getSid();
-//						}
-//						System.out.println("[" + fileName +  "]  下载开始.....");
-//						String filePath = savePath + fileName + ".mp3";
-//						if(!WebRequestUtil.downloadFile(downloadUrl, filePath)) {
-//							System.out.println("[" + fileName + "]  下载失败!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//						} else {
-//							System.out.println("[" + fileName +  "]  下载成功!");
-//						}
-//					}
-//				}
-//			}
-//			if(!isfind){
-//				break;
-//			}
-//		}
-//		System.out.println("下载了" + i + "首歌！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！6！");
-//	}
-//
-//	public static void setSongs(List<SongInfo> songInfos) {
-//		songs = songInfos;
-//	}
 
 }
